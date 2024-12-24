07a7766cc3a6b73fbfa4adbd9e28f497a667a00f
Alexey Panfilov <me@dzarlax.dev>
2024-12-24 19:07:52 +0100

    Update service worker URL for improved deployment consistency

--- src/client/serviceWorkerRegistration.js
+++ src/client/serviceWorkerRegistration.js
@@ -4,7 +4,7 @@
 export function register() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        const swUrl = `https://transport-api.dzarlax.dev/service-worker.js`;
+        const swUrl = `https://transport.dzarlax.dev/service-worker.js`;
         navigator.serviceWorker
           .register(swUrl)
           .then((registration) => {
