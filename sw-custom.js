f8d65a748ca71866c545a587639ebb19ef6e1a0e
dzarlax <dzarlax@users.noreply.github.com>
2026-03-25 10:32:43 +0000

    deploy: 1ef5324322ffe2e5aa23ed93eee46dc01c1be070

--- sw-custom.js
+++ sw-custom.js
@@ -1,5 +1,5 @@
 // Custom Service Worker для City Dashboard PWA
-const CACHE_NAME = 'city-dashboard-mn5uhpp8';
+const CACHE_NAME = 'city-dashboard-mn5wmyda';
 
 // Определяем SERVER_IP в зависимости от окружения
 const SERVER_IP = (() => {
